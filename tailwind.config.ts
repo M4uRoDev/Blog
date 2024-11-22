0adecb4666a7632a0c8df1d1016b89e28a527cd2
M4uRoDev <mauro.poblete.c@gmail.com>
2024-11-22 18:48:45 -0300

    + files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,12 +2,16 @@ import type { Config } from "tailwindcss";
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['GeistSans', 'sans-serif'],
+        mono: ['GeistMono', 'monospace'],
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
